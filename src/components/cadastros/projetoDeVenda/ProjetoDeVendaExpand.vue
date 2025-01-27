71d03f7e3fd2ebeb1b3bf390e8fe071d503f8e71
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2025-01-27 13:24:34 -0300

    Ajustes

--- src/components/cadastros/projetoDeVenda/ProjetoDeVendaExpand.vue
+++ src/components/cadastros/projetoDeVenda/ProjetoDeVendaExpand.vue
@@ -18,20 +18,21 @@
     Dados do Produto
     <v-icon>mdi-file-account</v-icon>
   </h4>
-  <v-row dense style="height: 170px" class="border-md pa-2">
+  <v-row dense style="height: 200px" class="border-md pa-2">
     <v-col v-for="(projeto, index) in projetoData.projetoProdutos" :key="index" cols="2" class="d-flex flex-wrap flex-column mr-16 h-50">
-      <div class="d-flex flex-wrap flex-column mr-16" style="height: 150px; border: 2px solid white; width: 240px">
-        <div class="d-flex flex-column flex-wrap" align="start" style="width: 100%; height: 100%">
-          <p style="margin-right: -100px; padding-left: 5px">Produto {{ index + 1 }}</p>
-          <v-col cols="8" align="start" style="margin-left: -7px; margin-top: -6px">
-            <span>Descrição:</span>
+      <div class="d-flex flex-wrap flex-column mr-16" style="height: 175px; border: 2px solid white; width:390px" align="start">
+        <p class="pl-3 pt-2" style="margin-right: -100px; font-weight: bold; width: 150px;;">Produto {{ index + 1 }}</p>
+
+        <div class="d-flex container mt-2" align="start" style="width: 100%; height: 100%">
+          <v-col cols="12" class="mt-6 pt-2 container" align="start" style="margin-left: -50px">
+            <span style="font-weight: bold;">Descrição:</span>
             <p>{{ projeto.produto.descricao }}</p>
             <br />
             <span>Unidade:</span>
             <p>{{ projeto.produto.unidade }}</p>
           </v-col>
-          <v-col cols="9" align="start" class="mt-4" style="margin-left: -50px">
-            <span>Preço Médio:</span>
+          <v-col cols="9" align="start" class="mt-5 container" style="margin-left: -130px; ">
+            <span style="font-weight: bold;">Preço Médio:</span>
             <p>{{ formatPrice(projeto.produto.precoMedio) }}</p>
             <br />
             <span>Quantidade:</span>
@@ -235,4 +236,10 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+
+.container {
+  display: grid;
+  grid-gap: 3px;
+}
+</style>
