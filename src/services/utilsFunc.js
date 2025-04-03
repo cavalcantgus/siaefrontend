9430e7013af53b63788646f87944674cda61ad64
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2025-04-03 10:33:32 -0300

    Implementando notificações para pagamento

--- src/services/utilsFunc.js
+++ src/services/utilsFunc.js
@@ -1,5 +1,22 @@
 import axios from "@/services/axios.js";
-const pendingUsers = []
+const pendingUsers = [];
+
+async function getNotificationsByUser() {
+  const data = JSON.parse(localStorage.getItem("user"));
+  
+  if (!data || !data.user || !data.user.id) {
+    console.error("Usuário não encontrado no localStorage");
+    return [];
+  }
+
+  try {
+    const response = await axios.get(`/public/notificacoes/${data.user.id}`);
+    return response.data;  // Retorna os dados diretamente
+  } catch (error) {
+    console.error("Erro ao buscar notificações:", error);
+    return []; // Retorna um array vazio em caso de erro
+  }
+}
 
 function getRoleFromToken() {
   const token = localStorage.getItem("token");
@@ -10,47 +27,50 @@ function getRoleFromToken() {
   try {
     const base64payload = token.split(".")[1];
     if (!base64payload) {
-        console.error("Formato inválido de token");
-        return null;
+      console.error("Formato inválido de token");
+      return null;
     }
 
     const decodedPayload = atob(base64payload.replace(/-/g, "+").replace(/_/g, "/"));
     const payload = JSON.parse(decodedPayload);
-
+    console.log("PAYLOAD", payload);
     return payload.role || "";
-     
-    } catch (error) {
-        console.error("Erro ao decodificar o token:", error);
-        return null;
-    }
+  } catch (error) {
+    console.error("Erro ao decodificar o token:", error);
+    return null;
+  }
 }
 
 async function getUsers() {
   const response = await axios.get("public/users");
   console.log(response.data);
-  return response.data
+  return response.data;
 }
 
 async function getPendingUsers() {
-  try{
-      await axios.get('/admin/pending-users', {
-          headers: {
-              Authorization: `Bearer ${localStorage.getItem('token')}`
-          }
-      }).then(response => {
-          this.pendingUsers = response.data
-      }).catch(error => {
-          console.log("Error: ", error)
+  try {
+    await axios
+      .get("/admin/pending-users", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
       })
-  } catch(error) {
-      console.log("Error: ", error)
+      .then((response) => {
+        this.pendingUsers = response.data;
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  } catch (error) {
+    console.log("Error: ", error);
   }
 
-  return this.pendingUsers
-} 
+  return this.pendingUsers;
+}
 
 export default {
+  getNotificationsByUser,
   getRoleFromToken,
   getUsers,
-  getPendingUsers
-}
+  getPendingUsers,
+};
