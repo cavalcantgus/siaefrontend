a0b0034583681014c1db50add1fc9afaed447949
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2025-04-18 19:00:04 -0300

    Tela de perfil de produtor responsiva

--- src/components/menus/ProfileMenu.vue
+++ src/components/menus/ProfileMenu.vue
@@ -1,56 +1,59 @@
 <template>
-    <v-menu offset="12">
-        <template #activator="{ props }">
-            <v-btn icon v-bind="props" class="profile">
-                <v-icon>mdi-account-circle</v-icon>
-            </v-btn>
-        </template>
-        <v-card class="custom-card" min-width="200px" outlined color="#57a340" variant="elevated" dark>
-            <v-list class="custom-list" :lines="false" density="compact" nav>
-            <v-list-item v-for="(item, index) in profileItems" :key="index"  class="custom-item text-white" @click="handleAction(item)">
-                <v-list-item-title><v-icon>{{ item.icon }}</v-icon> {{ item.title }}</v-list-item-title>
-            </v-list-item>
-        </v-list>
-        </v-card>
-    </v-menu>
+  <v-menu offset="12">
+    <template #activator="{ props }">
+      <v-btn icon v-bind="props" class="profile">
+        <v-avatar size="40" class="ml-auto">
+          <v-img alt="John" src="https://cdn.vuetifyjs.com/images/john.jpg" />
+        </v-avatar>
+      </v-btn>
+    </template>
+    <v-card class="custom-card" min-width="200px" outlined color="#57a340" variant="elevated" dark>
+      <v-list class="custom-list" :lines="false" density="compact" nav>
+        <v-list-item v-for="(item, index) in profileItems" :key="index" class="custom-item text-white" @click="handleAction(item)">
+          <v-list-item-title
+            ><v-icon>{{ item.icon }}</v-icon> {{ item.title }}</v-list-item-title
+          >
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-menu>
 </template>
 
 <script>
 export default {
-    name: 'ProfileMenu',
-    props: {
-        profileItems: {
-            type: Array,
-            required: true
-        }
+  name: "ProfileMenu",
+  props: {
+    profileItems: {
+      type: Array,
+      required: true,
     },
-    methods: {
-        handleAction(item) {
-            if(item.action ===  "logout") {
-                this.logout()
-            } else if(item.to) {
-                this.$router.push(item.to)
-            }
-        },
+  },
+  methods: {
+    handleAction(item) {
+      if (item.action === "logout") {
+        this.logout();
+      } else if (item.to) {
+        this.$router.push(item.to);
+      }
+    },
 
-        logout() {
-            console.log("SAIR")
-            localStorage.removeItem("token")
-            this.$router.push('/login')
-        }
-    }
-}
+    logout() {
+      console.log("SAIR");
+      localStorage.removeItem("token");
+      this.$router.push("/login");
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 .profile {
   background-color: transparent;
   display: flex;
   justify-self: flex-end;
   align-self: center;
   margin-right: 40px;
-  cursor: pointer
+  cursor: pointer;
 }
 .custom-card {
   background-color: #57a340;
@@ -64,7 +67,7 @@ export default {
 .custom-item {
   color: white;
   transition: background-color 0.3s ease;
-  cursor: pointer
+  cursor: pointer;
 }
 
 .custom-item:hover {
@@ -72,14 +75,13 @@ export default {
 }
 
 .v-btn:focus,
-.v-btn:active
- {
+.v-btn:active {
   outline: none !important;
-  background-color: #57a340 !important; 
-  box-shadow: none !important;      
+  background-color: #57a340 !important;
+  box-shadow: none !important;
 }
 
 .v-btn:focus-visible {
-  outline: none !important; 
+  outline: none !important;
 }
 </style>
